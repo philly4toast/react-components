988328e1b533eb55a8d8e8876f280c7b1046fd96
Phillip Youn <phillipyoun@gmail.com>
2020-03-03 16:30:01 -0800

    Review React components

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-// TODO
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// var Apples =()=>(
+//   <li>Apples</li>
+// )
+
+// var Carrots =()=>(
+//   <li>Carrots</li>
+// )
+
+// var onListItemClick = (event) => {
+//   console.log('clickitty')
+// }
+
+// var GroceryListItem =(props)=> (
+//   <ul>
+//   <li onClick={onListItemClick}>{props.items[0]}</li>
+//   <li>{props.items[1]}</li>
+//   <li>{props.items[2]}</li>
+//   </ul>
+// )
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bold: false
+    };
+}
+
+onItemHover() {
+  this.setState({
+    bold: !this.state.bold
+  });
+}
+
+render() {
+  var style = {
+    fontWeight: this.state.bold ? '800' : '200'
+  }
+  return <li style={style} onMouseOver={this.onItemHover.bind(this)} onMouseOut={this.onItemHover.bind(this)}>{this.props.item}</li>
+}
+
+
+}
+
+
+
+
+
+
+
+var GroceryList = (props) => {
+  return (
+<ul>
+{props.items.map(item => <GroceryListItem item={item}/>)}
+</ul>
+  )
+}
+
+ReactDOM.render(<GroceryList items={['Chili Picante', 'Happy', 'Salt']}/>, document.getElementById('app'))
